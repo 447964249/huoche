1edfab4a4da5a601b5f51d38d324da32b8236bc3
447964249 <laiyawen@qq.com>
2016-08-01 23:01:06 +0800

    修改json数据转换方法

--- src/cn/ucai/fulicenter/utils/Utils.java
+++ src/cn/ucai/fulicenter/utils/Utils.java
@@ -81,8 +81,17 @@ public class Utils {
         Result result = new Result();
         try {
             JSONObject jsonObject = new JSONObject(jsonStr);
-            result.setRetCode(jsonObject.getInt("retCode"));
-            result.setRetMsg(jsonObject.getBoolean("retMsg"));
+            if (!jsonObject.isNull("reCode")){
+                result.setRetCode(jsonObject.getInt("retCode"));
+            }else  if (!jsonObject.isNull("msg")){
+                result.setRetCode(jsonObject.getInt("msg"));
+            }
+
+            if (!jsonObject.isNull("retMsg")){
+                result.setRetMsg(jsonObject.getBoolean("retMsg"));
+            }else if (!jsonObject.isNull("result")){
+                result.setRetMsg(jsonObject.getBoolean("result"));
+            }
             if(!jsonObject.isNull("retData")) {
                 JSONObject jsonRetData = jsonObject.getJSONObject("retData");
                 if (jsonRetData != null) {
@@ -102,7 +111,28 @@ public class Utils {
                         return result;
                     }
                 }
+
+            }else {
+                if (jsonObject!= null) {
+                    Log.e("Utils", "jsonObject=" + jsonObject);
+                    String date;
+                    try {
+                        date = URLDecoder.decode(jsonObject.toString(), I.UTF_8);
+                        Log.e("Utils", "jsonObject=" + date);
+                        T t = new Gson().fromJson(date, clazz);
+                        result.setRetData(t);
+                        return result;
+
+                    } catch (UnsupportedEncodingException e1) {
+                        e1.printStackTrace();
+                        T t = new Gson().fromJson(jsonObject.toString(), clazz);
+                        result.setRetData(t);
+                        return result;
+                    }
+                }
+
             }
+
             return result;
         }catch (Exception e){
             e.printStackTrace();
@@ -115,8 +145,21 @@ public class Utils {
         Log.e("Utils","jsonStr="+jsonStr);
         try {
             JSONObject jsonObject = new JSONObject(jsonStr);
-            result.setRetCode(jsonObject.getInt("retCode"));
-            result.setRetMsg(jsonObject.getBoolean("retMsg"));
+
+            if (!jsonObject.isNull("reCode")){
+                result.setRetCode(jsonObject.getInt("retCode"));
+            }else  if (!jsonObject.isNull("msg")){
+                result.setRetCode(jsonObject.getInt("msg"));
+            }
+
+            if (!jsonObject.isNull("retMsg")){
+                result.setRetMsg(jsonObject.getBoolean("retMsg"));
+            }else if (!jsonObject.isNull("result")){
+                result.setRetMsg(jsonObject.getBoolean("result"));
+            }
+
+
+
             if(!jsonObject.isNull("retData")) {
                 JSONArray array = jsonObject.getJSONArray("retData");
                 if (array != null) {
