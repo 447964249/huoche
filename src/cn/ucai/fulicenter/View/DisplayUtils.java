910b8f2a6949f5d7c04fc7800ddc3117223d32b7
447964249 <laiyawen@qq.com>
2016-08-03 20:23:23 +0800

    返回按钮类

---
+++ src/cn/ucai/fulicenter/View/DisplayUtils.java
@@ -0,0 +1,24 @@
+package cn.ucai.fulicenter.View;
+
+/**
+ * Created by Administrator on 2016/8/3.
+ */
+
+        import android.app.Activity;
+        import android.view.View;
+
+        import cn.ucai.fulicenter.R;
+
+/**
+ * Created by clawpo on 16/8/3.
+ */
+public class DisplayUtils {
+    public static void initBack(final Activity activity){
+        activity.findViewById(R.id.backClickArea).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                activity.finish();
+            }
+        });
+    }
+}
